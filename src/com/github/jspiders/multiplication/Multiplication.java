9ee69afd88faa0ee617b09f453bf091c34886d4f
Siddiq Pasha <siddiqpashaha1997@gmail.com>
2020-03-05 14:00:16 +0530

    Multiplication of 3 numbers

---
+++ src/com/github/jspiders/multiplication/Multiplication.java
@@ -0,0 +1,18 @@
+package com.github.jspiders.multiplication;
+
+public class Multiplication 
+{
+	public static int multiply(int x,int y,int z)
+	{
+		
+		int a=x*y*z;
+		return a;
+	}
+	public static void main(String[] args)
+	{
+		int b=multiply(20,10,5);
+		System.out.println(b);
+		
+	}
+
+}
