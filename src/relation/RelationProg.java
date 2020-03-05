65f81e51631e01081dd37ef3badfd17044640ed5
UdayKumarAJ <55905401+UdayKumarAJ@users.noreply.github.com>
2020-03-05 14:00:35 +0530

    compare of two integers

---
+++ src/relation/RelationProg.java
@@ -0,0 +1,44 @@
+package relation;
+
+import java.util.Scanner;
+
+public class RelationProg 
+{
+	
+	public static void comparision()
+	{
+		Scanner sc = null;
+		sc = new Scanner(System.in);
+		
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	
+	if (a==b) 
+	{
+		System.out.println("The Given two numbers are equal");
+	}
+	else if(a<b)
+	{
+		System.out.println("The"+a+"is smaller than"+b);
+	}
+	else
+	{
+		System.out.println("The"+a+"is greater than"+b);
+	}
+	
+	if (sc!=null) 
+	{
+	sc.close();	
+	}
+		
+	}
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("Comparision of two integers Program starts...");
+		
+		comparision();
+		System.out.println("Comparision of two integers Program ends...");
+	}
+
+}
