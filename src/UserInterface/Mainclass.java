17ece85394838718268baa6c3c5547a7c7e85366
Aditya Murthy <Aditya Murthy@ADITYAMURTHY>
2020-03-05 14:00:13 +0530

    Division of three Numbers

--- src/UserInterface/Mainclass.java
+++ src/UserInterface/Mainclass.java
@@ -4,9 +4,7 @@ public class Mainclass {
 
 	public static void main(String[] args)
 	{
-		
 		System.out.println("Welcome to Project Calculator");
-
 	}
 
 }
---
+++ src/com/jspiders/calculator/division/Divide3Numbers.java
@@ -0,0 +1,16 @@
+package com.jspiders.calculator.division;
+
+public class Divide3Numbers {
+
+	public static void main(String[] args) {
+		
+		int n1 = 10;
+		int n2 = 20;
+		int n3 = 30;
+		
+		int res = 10/20/30;
+		System.out.println(res);
+
+	}
+
+}
