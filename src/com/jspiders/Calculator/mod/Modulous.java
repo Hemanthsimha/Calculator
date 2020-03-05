20324ee08cc84918f2b99d9787e32f591bb9a160
Admin <Admin@PHEONIX>
2020-03-05 14:01:03 +0530

    Created new java package in calculator and created class to find the modulus of two numbers

---
+++ src/com/jspiders/Calculator/mod/Modulous.java
@@ -0,0 +1,18 @@
+package com.jspiders.Calculator.mod;
+
+public class Modulous 
+{
+
+	public static void modoftwo(int a,int b)
+	{
+		int mod;
+		mod=a%b;
+		System.out.println(mod);
+	}
+	
+	public static void main(String[] args) 
+	{
+       modoftwo(30,7);
+	}
+
+}
