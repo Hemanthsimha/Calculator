4d859cf4bb7205443a9f8aa082eeb0161d856c56
PC <PC@HARINI>
2020-03-05 14:00:25 +0530

    relation operations

---
+++ src/com/jspiders/calc/RelationOpts.java
@@ -0,0 +1,19 @@
+package com.jspiders.calc;
+
+public class RelationOpts {
+
+	public static void main(String[] args) 
+	{
+		int a =10;
+		int b =20;
+		if(a==b)
+		{
+		System.out.println("a equals b");
+		}
+		if(a!=b)
+		{
+			System.out.println("a not equals b");
+		}
+	}
+
+}
